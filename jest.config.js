9b35943e1276363248957dfe947ef3497100dbbc
Drey0911 <andrey0911a@gmail.com>
2025-11-26 13:24:20 -0500

    Jest edit

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-vector-icons|react-native-gesture-handler|react-native-safe-area-context|react-native-screens)/)'
+    'node_modules/(?!react-native|@react-native|@react-navigation|react-navigation|react-native-vector-icons|react-native-gesture-handler|react-native-safe-area-context|react-native-screens)',
   ],
   setupFiles: ['react-native-gesture-handler/jestSetup']
 };
