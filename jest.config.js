5e6bc92e81f573c969c88bc5ecd85aa39a54b5be
Drey0911 <andrey0911a@gmail.com>
2025-11-26 13:36:47 -0500

    Edit

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,27 @@
 module.exports = {
   preset: 'react-native',
+  
+  // Asegura que Jest transforme estos módulos
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native|@react-navigation|react-navigation|react-native-reanimated|react-native-gesture-handler|react-native-safe-area-context|react-native-screens|react-native-vector-icons|@react-native-async-storage|react-native-worklets)/)',
+    'node_modules/(?!(react-native|@react-native(-community)?|@react-navigation|react-native-reanimated|react-native-gesture-handler|react-native-safe-area-context|react-native-screens|react-native-vector-icons|@react-native-async-storage|react-native-worklets|react-native-linear-gradient)/)',
   ],
+  
   setupFiles: ['react-native-gesture-handler/jestSetup'],
   
+  // Fuerza a Jest a usar las versiones CommonJS
   moduleNameMapper: {
-    '^@react-navigation/native$': '@react-navigation/native/lib/commonjs/index.js',
-    '^@react-navigation/stack$': '@react-navigation/stack/lib/commonjs/index.js',
-    '^@react-navigation/(.*)$': '@react-navigation/$1/lib/commonjs/index.js',
-    
+    '^@react-navigation/native$': '@react-navigation/native/lib/commonjs/index',
+    '^@react-navigation/stack$': '@react-navigation/stack/lib/commonjs/index',
+    '^@react-navigation/elements$': '@react-navigation/elements/lib/commonjs/index',
+    '^@react-navigation/core$': '@react-navigation/core/lib/commonjs/index',
     'react-native-linear-gradient': 'react-native-linear-gradient/jest/react-native-linear-gradient.js'
-  }
+  },
+  
+  // Configuración adicional para resolver módulos correctamente
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  
+  // Asegura que Jest use la configuración correcta de resolución
+  resolver: undefined,
+  
+  testEnvironment: 'node',
 };
