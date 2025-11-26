aa3564c7463293cda0af1850c8609e4c664b699a
Drey0911 <andrey0911a@gmail.com>
2025-11-26 13:32:41 -0500

    Edit

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,11 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native|@react-navigation|react-navigation|react-native-reanimated|react-native-gesture-handler|react-native-safe-area-context|react-native-screens|react-native-vector-icons|@react-native-async-storage|react-native-worklets)/)',
   ],
-  setupFiles: ['react-native-gesture-handler/jestSetup']
+  setupFiles: ['react-native-gesture-handler/jestSetup'],
+  
+  'moduleNameMapper': {
+    '^@react-navigation/native$': '<rootDir>/node_modules/@react-navigation/native/lib/commonjs/index.js',
+    
+    'react-native-linear-gradient': 'react-native-linear-gradient/jest/react-native-linear-gradient.js'
+  }
 };
